8d7ea801cff49a4e3f57a19075cbbe07e6944fbb
habip hakkioglu <hhakkioglu@gmail.com>
2023-11-15 17:15:48 +0100

    Update Navbar dropdown menu.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,33 +66,36 @@ export default function Navbar({darkMode, toggleDarkMode}) {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/register"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                      {
+                        !currentUser ? (
+                          <><Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/register"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Register
+                            </Link>
+                          )}
+                        </Menu.Item><Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                to="/login"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Login
+                              </Link>
                             )}
-                          >
-                            Register
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/login"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Login
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
+                          </Menu.Item></>
+                        ) 
+                        : 
+                        (<Menu.Item>
                         {({ active }) => (
                           <span 
                             onClick={() => logout()}
@@ -104,7 +107,9 @@ export default function Navbar({darkMode, toggleDarkMode}) {
                             Logout
                           </span>
                         )}
-                      </Menu.Item>
+                      </Menu.Item>)
+                      }
+                                   
                     </Menu.Items>
                   </Transition>
                 </Menu>
